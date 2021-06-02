360a79e2e063bf3a1dd43477f962fdc813973298
YasminEsmeralda <y.luetzelschwab@hotmail.com>
2021-06-02 14:52:34 +0200

    remove unused imports

--- spring-boot-healthily-infoM-project3/src/main/java/ch/zhaw/springboot/repositories/WorkoutRepository.java
+++ spring-boot-healthily-infoM-project3/src/main/java/ch/zhaw/springboot/repositories/WorkoutRepository.java
@@ -1,9 +1,6 @@
 package ch.zhaw.springboot.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import ch.zhaw.springboot.entities.Workout;
 
